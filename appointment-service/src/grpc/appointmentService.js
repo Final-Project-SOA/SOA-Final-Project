9bef2af3bfdb7ea16013b68b6ce3f0d75493acce
EyaBlkacem <eya.belkacem@polytechnicien.tn>
2026-05-16 15:16:19 +0200

    Ajout appointment creation et event publishing

--- appointment-service/src/grpc/appointmentService.js
+++ appointment-service/src/grpc/appointmentService.js
@@ -2,45 +2,48 @@ const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
 
-const db = require('../db/database');
+const eventBus = require('../kafka/eventBus');
 
 const PROTO_PATH = path.join(__dirname, '../../../proto/appointment.proto');
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
-const appointmentProto = grpc.loadPackageDefinition(packageDefinition).appointment;
+
+const appointmentProto =
+    grpc.loadPackageDefinition(packageDefinition).appointment;
+
+const appointments = [];
 
 function GetAppointments(call, callback) {
-    db.all('SELECT * FROM appointments', [], (err, rows) => {
-        if (err) {
-            return callback(err);
-        }
 
-        callback(null, { appointments: rows });
+    callback(null, {
+        appointments
     });
+
 }
 
 function CreateAppointment(call, callback) {
-    const { patientId, doctor, date } = call.request;
 
-    db.run(
-        'INSERT INTO appointments(patientId, doctor, date) VALUES (?, ?, ?)',
-        [patientId, doctor, date],
-        function (err) {
-            if (err) {
-                return callback(err);
-            }
+    const appointment = {
+        id: Date.now(),
+        patientId: call.request.patientId,
+        doctor: call.request.doctor,
+        date: call.request.date
+    };
 
-            callback(null, {
-                id: this.lastID,
-                patientId,
-                doctor,
-                date
-            });
-        }
+    appointments.push(appointment);
+
+    eventBus.emit(
+        'appointment-created',
+        appointment
     );
+
+    console.log('Appointment event published');
+
+    callback(null, appointment);
 }
 
 function main() {
+
     const server = new grpc.Server();
 
     server.addService(
@@ -55,10 +58,13 @@ function main() {
         '0.0.0.0:50052',
         grpc.ServerCredentials.createInsecure(),
         () => {
+
             console.log('Appointment gRPC running on 50052');
+
             server.start();
         }
     );
+
 }
 
 main();
